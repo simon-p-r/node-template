e64ff5c7bee9e5742b729a7b37cd23e531a81077
simon-p-r <simon.ricaldone@circabs.com>
2014-12-12 22:47:43 +0000

    Added vars for test

--- test/index.js
+++ test/index.js
@@ -3,3 +3,15 @@
 var Code = require('code');
 var Hapi = require('hapi');
 var Lab = require('lab');
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+var describe = lab.describe;
+var it = lab.it;
